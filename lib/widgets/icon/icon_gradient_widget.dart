a90b755e19fa961def07e580f0c4b39f756bb5e8
정원제 <specific0924@gmail.com>
2022-08-02 08:35:52 +0000

    Revert "Merge branch 'feature/landing_screen_design' into 'develop'"
    This reverts merge request !16

--- lib/widgets/icon/icon_gradient_widget.dart
+++ lib/widgets/icon/icon_gradient_widget.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 class IconGradientWidget extends StatelessWidget {
-  const IconGradientWidget(this.icon, this.size, this.gradient, {Key? key, this.scale = 1.2})
-      : super(key: key);
+  IconGradientWidget(
+      this.icon,
+      this.size,
+      this.gradient,
+      );
 
   final IconData icon;
   final double size;
   final Gradient gradient;
-  final double scale;
 
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
       child: SizedBox(
-        width: size * scale,
-        height: size * scale,
+        width: size * 1.2,
+        height: size * 1.2,
         child: Icon(
           icon,
           size: size,
