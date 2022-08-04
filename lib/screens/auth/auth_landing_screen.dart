0e5a5ee6a57a902d6a6fb962963fb46921381284
cryingInitial <specific0924@gmail.com>
2022-08-04 12:47:55 +0900

    Auth Landing page

--- lib/screens/auth/auth_landing_screen.dart
+++ lib/screens/auth/auth_landing_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:modak_flutter_app/assets/icons/light/LightIcons_icons.dart';
+import 'package:modak_flutter_app/constant/coloring.dart';
+import 'package:modak_flutter_app/constant/font.dart';
 import 'package:modak_flutter_app/screens/auth/reigster/auth_register_screen.dart';
-import 'package:modak_flutter_app/services/auth_service.dart';
 import 'package:modak_flutter_app/utils/auth_util.dart';
 import 'package:modak_flutter_app/utils/prefs_util.dart';
+import 'package:modak_flutter_app/widgets/auth/auth_introduction_widget.dart';
+import 'package:modak_flutter_app/widgets/button/button_main_widget.dart';
 
 class AuthLandingScreen extends StatefulWidget {
   const AuthLandingScreen({Key? key}) : super(key: key);
@@ -16,28 +20,120 @@ class _AuthLandingScreenState extends State<AuthLandingScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            TextButton(
-              onPressed: () async {
-                await AuthUtil.kakaoLogin(context);
-                if (PrefsUtil.getInt("provider_id") != null &&
-                    PrefsUtil.getString("provider") != null) {
-                  PrefsUtil.setBool("is_register_progress", true);
-
-                  Future(() => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AuthRegisterScreen())));
-                }
-              },
-              child: Text("카카오로 로그인"),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: Ink(
+            width: 32,
+            height: 32,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(0XFFF7F8F8),
             ),
-            TextButton(
-              onPressed: () async {
-                tokenLogin();
+            child: IconButton(
+              onPressed: () {
+                print("안녕");
               },
-              child: Text("통신 테스트"),
+              icon: Icon(
+                LightIcons.ArrowLeft2,
+                size: 16,
+                color: Coloring.gray_0,
+              ),
+            ),
+          ),
+          centerTitle: true,
+          title: Text("Modak",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: Font.size_largeText,
+                fontWeight: Font.weight_bold,
+              )),
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    AuthIntroductionWidget(
+                      name: "washer",
+                    ),
+                    AuthIntroductionWidget(
+                      name: "album",
+                    ),
+                    AuthIntroductionWidget(
+                      name: "chat",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: ButtonMainWidget(
+                title: "가족 방에 초대 받았습니다",
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 54,
+              ),
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                      onTap: () async {
+                        await AuthUtil.kakaoLogin(context);
+                        if (PrefsUtil.getInt("provider_id") != null &&
+                            PrefsUtil.getString("provider") != null) {
+                          PrefsUtil.setBool("is_register_progress", true);
+                          Future(() => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AuthRegisterScreen())));
+                        }
+                      },
+                      child: Image.asset(
+                        "lib/assets/kakao_login.png",
+                        width: 60,
+                        height: 60,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () async {
+                        await AuthUtil.kakaoLogin(context);
+                        if (PrefsUtil.getInt("provider_id") != null &&
+                            PrefsUtil.getString("provider") != null) {
+                          PrefsUtil.setBool("is_register_progress", true);
+                          Future(() => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AuthRegisterScreen())));
+                        }
+                      },
+                      child: Image.asset(
+                        "lib/assets/apple_login.png",
+                        width: 60,
+                        height: 60,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
---
+++ lib/widgets/auth/auth_introduction_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:modak_flutter_app/constant/coloring.dart';
+import 'package:modak_flutter_app/constant/font.dart';
+import 'package:modak_flutter_app/constant/shadowing.dart';
+
+class AuthIntroductionWidget extends StatefulWidget {
+  const AuthIntroductionWidget({Key? key, required this.name})
+      : super(key: key);
+
+  final String name;
+
+  @override
+  State<AuthIntroductionWidget> createState() => _AuthIntroductionWidgetState();
+}
+
+class _AuthIntroductionWidgetState extends State<AuthIntroductionWidget> {
+  final data = {
+    "washer": {
+      "title": "집안일",
+      "des": "집안일을 공유하고 도와주세요",
+      "imagePath": "lib/assets/img_graphic_washer.png",
+      "gradient": Coloring.main,
+      "shadow": Shadowing.yellow,
+    },
+    "album": {
+      "title": "가족 앨범",
+      "des": "가족들과의 추억을 저장하세요",
+      "imagePath": "lib/assets/img_graphic_album.png",
+      "gradient": Coloring.sub_purple,
+      "shadow": Shadowing.yellow,
+    },
+    "chat": {
+      "title": "공유",
+      "des": "가족들만의 소통 공간을 이용해보세요",
+      "imagePath": "lib/assets/img_graphic_chat.png",
+      "gradient": Coloring.sub_blue,
+      "shadow": Shadowing.yellow,
+    },
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 7.5),
+      decoration: BoxDecoration(
+          gradient: data[widget.name]!["gradient"] as LinearGradient,
+          borderRadius: BorderRadius.circular(22)),
+      child: Padding(
+        padding:
+            const EdgeInsets.only(top: 28, right: 16, bottom: 24, left: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              width: 168,
+              height: 122,
+              data[widget.name]!["imagePath"] as String,
+            ),
+            SizedBox(
+                width: 168,
+                height: 22,
+                child: Text(
+                  data[widget.name]!["title"] as String,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: Font.size_mediumText,
+                    fontWeight: Font.weight_semiBold,
+                  ),
+                )),
+            SizedBox(
+                width: 168,
+                height: 30,
+                child: Text(
+                  "${data[widget.name]!["des"] as String}\n",
+                  style: TextStyle(
+                    color: Coloring.gray_10,
+                    fontSize: Font.size_smallText,
+                    fontWeight: Font.weight_medium,
+                  ),
+                  maxLines: 2,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
