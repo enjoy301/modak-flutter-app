9bd8c7e42f4842d53f46feeb189d1f81335095b9
cryingInitial <specific0924@gmail.com>
2022-10-21 12:21:33 +0900

    Feature: 앱 배포

--- lib/ui/auth/auth_landing_screen.dart
+++ lib/ui/auth/auth_landing_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:modak_flutter_app/constant/font.dart';
 import 'package:modak_flutter_app/ui/auth/auth_landing_VM.dart';
@@ -60,46 +62,52 @@ class _AuthLandingScreenState extends State<AuthLandingScreen> {
                         width: 60,
                         height: 60,
                       ),
-                      Text("카카오 계정으로 로그인", style: TextStyle(
-                        color: Colors.black,
-                        fontSize: Font.size_largeText,
-                        fontWeight: Font.weight_semiBold,
-                      ),)
-                    ],
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () => provider.onSocialClick(context, "APPLE"),
-                child: Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  margin: EdgeInsets.only(right: 30, bottom: 50, left: 30),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(48),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 18),
-                        child: Image.asset(
-                          "lib/assets/images/auth/apple_icon.png",
-                          width: 30,
-                          height: 30,
+                      Text(
+                        "카카오 계정으로 로그인",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: Font.size_largeText,
+                          fontWeight: Font.weight_semiBold,
                         ),
-                      ),
-                      Text("애플 계정으로 로그인", style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Font.size_largeText,
-                        fontWeight: Font.weight_semiBold,
-                      ),)
+                      )
                     ],
                   ),
                 ),
               ),
-
+              if (!Platform.isAndroid)
+                GestureDetector(
+                  onTap: () => provider.onSocialClick(context, "APPLE"),
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    margin: EdgeInsets.only(right: 30, bottom: 50, left: 30),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(48),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 18),
+                          child: Image.asset(
+                            "lib/assets/images/auth/apple_icon.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                        Text(
+                          "애플 계정으로 로그인",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Font.size_largeText,
+                            fontWeight: Font.weight_semiBold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
